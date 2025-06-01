4a880406ef6f93b8bcaafbf3f4e384703423b0de
joaoLouceiro <jrlouceiro@live.com>
2025-06-01 11:16:02 +0100

    Stole some LazyVim keybinds

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,30 +1,73 @@
--- Call vim.keymap.set
--- @param t (table)
---  - t[1] *required*: lhs mapping 
---  - t[2] *required*: rhs mapping
---  - mode *optional*: defaults to 'n'
---  - opts *optional*: table with options
-local function SetKeymap(t)
-    if t[1] == nil or type(t[1]) ~= 'string' then
-        error("No lhs")
-    elseif t[2] == nil or type(t[2]) ~= 'string' then
-        error("No rhs")
-    end
+local function map(mode, lhs, rhs, opts)
     local options = { noremap = true, silent = true }
-    if t.opts then
-        options = vim.tbl_extend("force", options, t.opts)
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
     end
-    vim.keymap.set(t.mode or 'n', t[1], t[2], options)
+    if type(mode) == "table" then
+        for _, value in ipairs(mode) do
+            vim.keymap.set(value, lhs, rhs, options)
+        end
+    else
+        vim.keymap.set(mode, lhs, rhs, options)
+    end
 end
 
--- vim.g.mapleader = " "
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
-SetKeymap({'<C-h>', '<C-w><C-h>'})
-SetKeymap({'<C-j>', '<C-w><C-j>'})
-SetKeymap({'<C-k>', '<C-w><C-k>'})
-SetKeymap({'<C-l>', '<C-w><C-l>'})
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
-SetKeymap({ '<C-_>', 'gcc', mode='n', opts = { remap = true }})
-SetKeymap({ '<C-_>', 'gc', mode='v', opts = { remap = true }})
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
-SetKeymap({ '<C-s>', '<cmd>w<cr>'})
+-- Move Lines
+map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+-- Comments - some shells interpret <C-/> as <C-_>
+map('n',  '<C-/>', 'gcc', { remap = true })
+map('n',  '<C-_>', 'gcc', { remap = true })
+map('v',  '<C-/>', 'gc', { remap = true })
+map('v',  '<C-_>', 'gc', { remap = true })
+
+-- buffers
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+
+-- Clear search
+-- Clear search and stop snippet on escape
+map({ "i", "n", "s" }, "<esc>", function()
+  vim.cmd("noh")
+  return "<esc>"
+end, { expr = true, desc = "Escape and Clear hlsearch" })
+
+-- save file
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+--keywordprg
+map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- quit
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
