f805f3cc57aa3b10f21a494c7d5caa58510ddada
joaoLouceiro <jrlouceiro@live.com>
2025-06-02 18:54:35 +0100

    Fix duplicate lua_lsp

--- .config/nvim/lua/plugins/LSP.lua
+++ .config/nvim/lua/plugins/LSP.lua
@@ -2,17 +2,17 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function ()
-            local lspconfig = require('lspconfig')
-
-            lspconfig.lua_ls.setup {
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { 'vim' }
-                        }
-                    }
-                }
-            }
+            -- local lspconfig = require('lspconfig')
+            --
+            -- lspconfig.lua_ls.setup {
+            --     settings = {
+            --         Lua = {
+            --             diagnostics = {
+            --                 globals = { 'vim' }
+            --             }
+            --         }
+            --     }
+            -- }
         end
     },
     {
